4fb6e7977cd5b27910700ed7874a979f0298fb90
Heather Piwowar <hpiwowar@gmail.com>
2021-12-15 11:58:35 -0800

    fix error printout

--- models/work.py
+++ models/work.py
@@ -50,7 +50,7 @@ def call_sagemaker_bulk_lookup_new_work_concepts(rows):
     api_url = "https://4rwjth9jek.execute-api.us-east-1.amazonaws.com/api/"
     r = requests.post(api_url, json=json.dumps(data_list), headers=headers)
     if r.status_code != 200:
-        print("error: status code {r}")
+        print(f"error: status code {r}")
         return []
 
     api_json = r.json()
