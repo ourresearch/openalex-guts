eb395ce70ff5eeb4d89efafab4d2fe10cdb1c7fa
Heather Piwowar <hpiwowar@gmail.com>
2021-12-22 18:05:50 -0800

    try again

--- app.py
+++ app.py
@@ -77,9 +77,9 @@ app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = True  # as instructed, to suppres
 app.config['SQLALCHEMY_ECHO'] = (os.getenv("SQLALCHEMY_ECHO", False) == "True")
 # app.config['SQLALCHEMY_ECHO'] = True
 
-app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # don't use this though, default is unclear, use binds
+# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")  # don't use this though, default is unclear, use binds
 # app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL_OPENALEX_REDSHIFT_BASE")  # don't use this though, default is unclear, use binds
-# app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL_OPENALEX_REDSHIFT")  # don't use this though, default is unclear, use binds
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL_OPENALEX_REDSHIFT")  # don't use this though, default is unclear, use binds
 app.config["SQLALCHEMY_BINDS"] = {
     "redshift_db": os.getenv("DATABASE_URL_OPENALEX_REDSHIFT")
     # "redshift_db": os.getenv("DATABASE_URL_OPENALEX_REDSHIFT_BASE")
