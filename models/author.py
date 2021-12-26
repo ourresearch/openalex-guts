4393d110a98514653d9a65baec09dd4b5f19d8b1
Heather Piwowar <hpiwowar@gmail.com>
2021-12-26 07:50:56 -0800

    switch order

--- models/author.py
+++ models/author.py
@@ -204,8 +204,8 @@ class Author(db.Model):
                 },
                 # "orcid_data_person": self.orcid_data_person,
                 "last_known_institution": self.last_known_institution.to_dict("minimum") if self.last_known_institution else None,
-                "x_concepts": self.concepts,
                 "counts_by_year": self.display_counts_by_year,
+                "x_concepts": self.concepts,
                 "works_api_url": f"https://api.openalex.org/works?filter=author.id:{self.openalex_id_short}",
                 "updated_date": self.updated_date
             })
