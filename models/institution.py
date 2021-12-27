2cb8614a4026bdc28884cf682dc2ee06eaf5c706
Heather Piwowar <hpiwowar@gmail.com>
2021-12-26 16:59:23 -0800

    fix ambiguity

--- models/institution.py
+++ models/institution.py
@@ -347,14 +347,14 @@ class Institution(db.Model):
         from models.concept import as_concept_openalex_id
 
         q = """
-            select ancestor_id as id, wikidata_id as wikidata, ancestor_name as display_name, ancestor_level as level, round(100 * count(distinct affil.paper_id)/institution.paper_count::float, 1) as score
+            select ancestor_id as id, concept.wikidata_id as wikidata, ancestor_name as display_name, ancestor_level as level, round(100 * count(distinct affil.paper_id)/institution.paper_count::float, 1) as score
             from mid.institution institution 
             join mid.affiliation affil on affil.affiliation_id=institution.affiliation_id            
             join mid.work_concept_for_api_mv wc on wc.paper_id=affil.paper_id
             join mid.concept_self_and_ancestors_view ancestors on ancestors.id=wc.field_of_study
             join mid.concept concept on concept.field_of_study_id=ancestor_id                        
             where affil.affiliation_id=:institution_id
-            group by ancestor_id, wikidata_id, ancestor_name, ancestor_level, institution.paper_count
+            group by ancestor_id, concept.wikidata_id, ancestor_name, ancestor_level, institution.paper_count
             order by score desc
             """
         rows = db.session.execute(text(q), {"institution_id": self.institution_id}).fetchall()
