6dbef9d8676248d610cc3fb14f43b121eee4aa88
Heather Piwowar <hpiwowar@gmail.com>
2022-01-01 14:57:31 -0800

    fix generate_inverted_index and add extract_citation_dois in case helpful

--- sql/f_generate_inverted_index.sql
+++ sql/f_generate_inverted_index.sql
@@ -7,13 +7,20 @@ STABLE
 AS $$
 	import re
 	import json
-	
-	# remove jat tags
-	abstract_string = re.sub('<jats:[^<]+>', '', abstract_string)
-	abstract_string = re.sub('</jats:[^<]+>', '', abstract_string)
-	
+	from collections import OrderedDict
+
+	# remove jat tags and unnecessary tags and problematic white space
+	abstract_string = re.sub('\b', ' ', abstract_string)
+	abstract_string = re.sub('\n', ' ', abstract_string)
+	abstract_string = re.sub('\t', ' ', abstract_string)
+	abstract_string = re.sub('<jats:[^<]+>', ' ', abstract_string)
+	abstract_string = re.sub('</jats:[^<]+>', ' ', abstract_string)
+	abstract_string = re.sub('<p>', ' ', abstract_string)
+	abstract_string = re.sub('</p>', ' ', abstract_string)
+	abstract_string = " ".join(re.split("\s+", abstract_string))
+
 	# build inverted index
-	invertedIndex = {}
+	invertedIndex = OrderedDict()
 	words = abstract_string.split()
 	for i in range(len(words)):
 		if words[i] not in invertedIndex:
@@ -24,9 +31,10 @@ AS $$
 		'InvertedIndex': invertedIndex,
 	}
 	
-	return json.dumps(result)
+	return json.dumps(result, ensure_ascii=False)
 
 $$LANGUAGE plpythonu;
+
 
 -- select abstract, f_generate_inverted_index_v1(abstract) from legacy.crossref_main_works where abstract != '' limit 100;
 -- select abstract, f_generate_inverted_index_v1(abstract) from legacy.pubmed_main_works where abstract != '' limit 100;
