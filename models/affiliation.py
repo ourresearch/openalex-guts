acd4aebbe7ea72653a937f31235096fdb126a3de
Heather Piwowar <hpiwowar@gmail.com>
2022-01-03 17:44:40 -0800

    stick with empty dicts unless have data

--- models/affiliation.py
+++ models/affiliation.py
@@ -27,7 +27,6 @@ class Affiliation(db.Model):
     def to_dict(self, return_level="full"):
         response = {}
 
-
         # author_position set in works
         if hasattr(self, "author_position"):
             response["author_position"] = self.author_position
@@ -35,8 +34,10 @@ class Affiliation(db.Model):
         # keep in this order so author_position is at the top
         response.update({"author": {}, "institution": {}})
 
-        response["author"] = {"id": self.author_id, "display_name": self.original_author, "orcid": None}
-        response["institution"] = {"id": self.affiliation_id, "display_name": self.original_affiliation, "ror": None, "country_code": None, "type": None}
+        if self.original_author:
+            response["author"] = {"id": self.author_id, "display_name": self.original_author, "orcid": None}
+        if self.original_affiliation:
+            response["institution"] = {"id": self.affiliation_id, "display_name": self.original_affiliation, "ror": None, "country_code": None, "type": None}
 
         # overwrite display name with better ones from these dicts if we have them
         if self.author:
