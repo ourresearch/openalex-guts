015c8e46e9e60f46a312bb71e1f87f5f4e1e65bf
Heather Piwowar <hpiwowar@gmail.com>
2022-02-07 08:49:39 -0800

    just show created date not full timestring

--- models/author.py
+++ models/author.py
@@ -186,8 +186,8 @@ class Author(db.Model):
                 "counts_by_year": self.display_counts_by_year,
                 "x_concepts": self.concepts,
                 "works_api_url": f"https://api.openalex.org/works?filter=author.id:{self.openalex_id_short}",
-                "updated_date": self.updated_date.isoformat()[0:10] if isinstance(self.updated_date, datetime.datetime) else self.updated_date,
-                "created_date": self.created_date.isoformat()[0:10] if isinstance(self.created_date, datetime.datetime) else self.created_date
+                "updated_date": self.updated_date.isoformat()[0:10] if isinstance(self.updated_date, datetime.datetime) else self.updated_date[0:10],
+                "created_date": self.created_date.isoformat()[0:10] if isinstance(self.created_date, datetime.datetime) else self.created_date[0:10]
             })
 
             # only include non-null IDs
