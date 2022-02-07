368e840d037761a82ec41b27f9dc687691b848ab
Heather Piwowar <hpiwowar@gmail.com>
2022-02-07 08:39:46 -0800

    add created date to api response

--- models/concept.py
+++ models/concept.py
@@ -508,7 +508,7 @@ class Concept(db.Model):
                 "counts_by_year": self.display_counts_by_year,
                 "works_api_url": f"https://api.openalex.org/works?filter=concepts.id:{self.openalex_id_short}",
                 "updated_date": self.updated_date.isoformat()[0:10] if isinstance(self.updated_date, datetime.datetime) else self.updated_date,
-                # "created_date": self.created_date.isoformat()[0:10] if isinstance(self.created_date, datetime.datetime) else self.created_date
+                "created_date": self.created_date.isoformat()[0:10] if isinstance(self.created_date, datetime.datetime) else self.created_date
             })
 
             # only include non-null IDs
