5bdb24820b30e27e43fefffaa5dcb188f8c4910d
Heather Piwowar <hpiwowar@gmail.com>
2022-05-08 22:07:51 -0700

    update to more recent

--- scripts/queue.py
+++ scripts/queue.py
@@ -222,12 +222,12 @@ class DbQueue(object):
                     select {id_field_name} 
                         from {queue_table} t1
                         where full_updated_date is not null
-                        and full_updated_date > '2022-03-01'
+                        and full_updated_date > '2022-05-01'
                         and NOT EXISTS (
                            SELECT 1
                            FROM   {insert_table} t2
                            WHERE  (t1.{id_field_name}=t2.id) and ((t1.full_updated_date is not null) and (t1.full_updated_date < t2.updated))
-                           and updated > '2022-03-01'
+                           and updated > '2022-05-01'
                            )      
                         -- order by random()
                         limit {chunk};
