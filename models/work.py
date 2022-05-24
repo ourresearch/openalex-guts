ef6a7cf93400dba619ecf1d23b516d9e69de9894
Heather Piwowar <hpiwowar@gmail.com>
2022-05-24 13:07:34 -0700

    handle long titles better

--- models/work.py
+++ models/work.py
@@ -497,9 +497,9 @@ class Work(db.Model):
 
         if not self.created_date:
             self.created_date = datetime.datetime.utcnow().isoformat()
-        self.original_title = record.title
-        self.paper_title = normalize_simple(record.title, remove_articles=False, remove_spaces=False)
-        self.unpaywall_normalize_title = record.normalized_title
+        self.original_title = record.title[:60000] if record.title else None
+        self.paper_title = normalize_simple(self.original_title, remove_articles=False, remove_spaces=False)
+        self.unpaywall_normalize_title = record.normalized_title[:60000] if record.normalized_title else None
         self.updated_date = datetime.datetime.utcnow().isoformat()
         self.full_updated_date = datetime.datetime.utcnow().isoformat()
 
