2f7a81c9946ea87e56a2c213018331308f3a90d6
Heather Piwowar <hpiwowar@gmail.com>
2022-06-10 10:17:14 -0700

    update to try sagemaker again if status 500

--- models/work.py
+++ models/work.py
@@ -174,26 +174,30 @@ class Work(db.Model):
         headers = {"X-API-Key": api_key}
         api_url = "https://t9a5o7qfy2.execute-api.us-east-1.amazonaws.com/api/" #for vesion with abstracts
 
-        r = requests.post(api_url, json=json.dumps([data]), headers=headers)
-        try:
-            response_json = r.json()
-            concept_names = response_json[0]["tags"]
-        except Exception as e:
-            print(f"error {e} in add_work_concepts with {self.id}, response {r}, called with {api_url} data: {data} headers: {headers}")
-            concept_names = None
+        number_tries = 0
+        while True:
+            r = requests.post(api_url, json=json.dumps([data]), headers=headers)
+
+            if r.status_code == 200:
+                try:
+                    response_json = r.json()
+                    concept_names = response_json[0]["tags"]
+                except Exception as e:
+                    print(f"error {e} in add_work_concepts with {self.id}, response {r}, called with {api_url} data: {data} headers: {headers}")
+                    concept_names = None
+
+            elif r.status_code == 500:
+                print(f"Error on try #{number_tries}, now trying again: Error back from API endpoint: {r} {r.status_code} {r.text} for input {data}")
+                number_tries += 1
+
+            else:
+                print(f"Error, not retrying: Error back from API endpoint: {r} {r.status_code} {r.text} for input {data}")
+                return
+
 
         self.concepts_for_related_works = []
         fields_of_study = []
         if concept_names:
-            # concept_names_string = "({})".format(", ".join(["'{}'".format(concept_name) for concept_name in concept_names]))
-            # q = """
-            # select field_of_study_id, display_name
-            # from mid.concept
-            # where lower(display_name) in {concept_names_string}
-            # """.format(concept_names_string=concept_names_string)
-            # matching_concepts = db.session.execute(text(q)).all()
-            # print(f"concepts that match: {matching_concepts}")
-            # matching_ids = [concept[0] for concept in matching_concepts]
             for i, concept_name in enumerate(concept_names):
                 score = response_json[0]["scores"][i]
                 field_of_study = response_json[0]["tag_ids"][i]
