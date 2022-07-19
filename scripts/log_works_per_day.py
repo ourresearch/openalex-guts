8113d842094e9aa03cb91b677fbb869db979eb77
Richard Orr <richard@ourresearch.org>
2022-07-19 16:35:19 -0400

    add script to log count of works created and updated per day

---
+++ scripts/log_works_per_day.py
@@ -0,0 +1,28 @@
+import datetime
+import os
+
+import requests
+from app import db, logger
+from sqlalchemy import text
+
+api_key = os.getenv('OPENALEX_API_KEY')
+
+yesterday = (
+    datetime.datetime.utcnow().date() - datetime.timedelta(days=1)
+).isoformat()
+
+num_created = requests.get(
+    f'http://api.openalex.org/works?filter=from_created_date:{yesterday}&api_key={api_key}'
+).json().get('meta').get('count')
+
+num_updated = requests.get(
+    f'http://api.openalex.org/works?filter=from_updated_date:{yesterday}&api_key={api_key}'
+).json().get('meta').get('count')
+
+logger.info({'day': yesterday, 'created': num_created, 'updated': num_updated})
+
+db.engine.execute(
+    text('insert into log.works_per_day (day, num_created, num_updated) values (:day, :created, :updated)').bindparams(
+        day=yesterday, created=num_created, updated=num_updated
+    ).execution_options(autocommit=True)
+)
