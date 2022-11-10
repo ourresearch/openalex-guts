c8e7516e626a76a915125dac9f9d4373ab45ded4
Richard Orr <richard@ourresearch.org>
2022-11-10 11:20:58 -0500

    replace Venue.apc_prices with apc_usd drop Venue.is_society_journal

--- models/venue.py
+++ models/venue.py
@@ -40,6 +40,7 @@ class Venue(db.Model):
     repository_id = db.Column(db.Text)
     type = db.Column(db.Text)
     apc_prices = db.Column(JSONB)
+    apc_usd = db.Column(JSONB)
     is_society_journal = db.Column(db.Boolean)
     societies = db.Column(JSONB)
     created_date = db.Column(db.DateTime)
@@ -176,8 +177,7 @@ class Venue(db.Model):
                     "mag": self.journal_id if self.journal_id < MAX_MAG_ID else None
                 },
                 "type": self.type,
-                "apc_prices": self.apc_prices,
-                "is_society_journal": self.is_society_journal,
+                "apc_usd": self.apc_usd,
                 "societies": self.societies,
                 "counts_by_year": self.display_counts_by_year,
                 "x_concepts": self.concepts,
