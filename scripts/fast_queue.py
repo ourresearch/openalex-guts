e31509f03b11ca4c1d20034b18c3d66802255e43
Richard Orr <richard@ourresearch.org>
2023-04-10 15:51:23 -0400

    drop backref on Work.funders

--- scripts/fast_queue.py
+++ scripts/fast_queue.py
@@ -185,6 +185,7 @@ def get_objects(entity_type, object_ids):
             selectinload(models.Work.references_unmatched).raiseload('*'),
             selectinload(models.Work.mesh),
             selectinload(models.Work.funders).selectinload(models.WorkFunder.funder).raiseload('*'),
+            selectinload(models.Work.funders).raiseload('*'),
             selectinload(models.Work.counts),
             selectinload(models.Work.counts_by_year).raiseload('*'),
             selectinload(models.Work.abstract),
