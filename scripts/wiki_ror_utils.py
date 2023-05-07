3ce354ce781941344e37b78dfaba056245233c9b
Casey M <caseym@gmail.com>
2023-05-07 17:42:07 -0500

    improve get_image_url

--- scripts/wiki_ror_utils.py
+++ scripts/wiki_ror_utils.py
@@ -266,24 +266,21 @@ def get_image_url(wikidata_id):
         wikidata_shortcode = normalize_wikidata_id(wikidata_id)
         wikidata_response = fetch_wikidata_response(wikidata_shortcode)
         if wikidata_response:
-            # try to get the logo first
-            claims = wikidata_response["entities"][wikidata_shortcode]["claims"]
-            if "P154" in claims:
-                try:
-                    wikidata_image_file = claims["P154"][0]["mainsnak"]["datavalue"][
-                        "value"
-                    ]
-                    wikidata_image_url = f"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/{wikidata_image_file}"
-                except KeyError:
-                    print(f"Error getting image for {wikidata_shortcode}")
+            try:
+                # try to get the logo first
+                claims = wikidata_response["entities"][wikidata_shortcode]["claims"]
+                if "P154" in claims:
+                        wikidata_image_file = claims["P154"][0]["mainsnak"]["datavalue"][
+                            "value"
+                        ]
+                        wikidata_image_url = f"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/{wikidata_image_file}"
 
-            # if no logo, try to get an image
-            elif "P18" in claims:
-                try:
-                    wikidata_image_file = claims["P18"][0]["mainsnak"]["datavalue"]["value"]
-                    wikidata_image_url = f"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/{wikidata_image_file}"
-                except KeyError:
-                    print(f"Error getting image for {wikidata_shortcode}")
+                # if no logo, try to get an image
+                elif "P18" in claims:
+                        wikidata_image_file = claims["P18"][0]["mainsnak"]["datavalue"]["value"]
+                        wikidata_image_url = f"https://commons.wikimedia.org/w/index.php?title=Special:Redirect/file/{wikidata_image_file}"
+            except KeyError:
+                print(f"Error getting image url for {wikidata_shortcode}")
     return wikidata_image_url
 
 
