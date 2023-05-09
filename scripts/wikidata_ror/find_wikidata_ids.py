3e4d0c5f3953d7ee4c693583e37ac9681e27ae94
Casey M <caseym@gmail.com>
2023-05-09 08:58:40 -0500

    improve find wikidata script

--- scripts/wikidata_ror/find_wikidata_ids.py
+++ scripts/wikidata_ror/find_wikidata_ids.py
@@ -1,9 +1,11 @@
-import requests
+from scripts.wikidata_ror.cached_session import cached_session
+from scripts.wikidata_ror.response import fetch_wikidata_response
 
 
 def find_wikidata_id(display_name, words_to_ignore=None, words_to_include=None):
     # search the wikidata API for the display_name
-    r = requests.get(
+    session = cached_session()
+    r = session.get(
         f"https://www.wikidata.org/w/api.php?action=wbsearchentities&search={display_name}&language=en&format=json"
     )
     if r.status_code == 200:
@@ -27,7 +29,8 @@ def find_wikidata_id(display_name, words_to_ignore=None, words_to_include=None):
 
 def find_wikidata_id_for_source(display_name):
     # search the wikidata API for the display_name
-    r = requests.get(
+    session = cached_session()
+    r = session.get(
         f"https://www.wikidata.org/w/api.php?action=wbsearchentities&search={display_name}&language=en&format=json"
     )
     if r.status_code == 200:
@@ -42,16 +45,13 @@ def find_wikidata_id_for_source(display_name):
                 or result["label"].lower() == display_name.lower()
             ):
                 wikidata_shortcode = result["id"]
-                r2 = requests.get(
-                    f"https://www.wikidata.org/w/api.php?action=wbgetclaims&entity={wikidata_shortcode}&format=json"
-                )
-                if r2.status_code == 200:
-                    response2 = r2.json()
+                response2 = fetch_wikidata_response(wikidata_shortcode)
+                if response2:
                     if (
-                        is_instance_of_scientific_journal(response2)
-                        or is_instance_of_online_database(response2)
-                        or has_issn_or_issnl(response2)
-                        or has_source_words_in_description(response2)
+                        is_instance_of_scientific_journal(response2, wikidata_shortcode)
+                        or is_instance_of_online_repository(response2, wikidata_shortcode)
+                        or has_issn_or_issnl(response2, wikidata_shortcode)
+                        or has_source_words_in_description(response2, wikidata_shortcode)
                     ):
                         wikidata_id = (
                             f"https://www.wikidata.org/entity/{wikidata_shortcode}"
@@ -65,9 +65,9 @@ def find_wikidata_id_for_source(display_name):
                         return None
 
 
-def is_instance_of_online_database(response):
-    claims = response.get("claims", {})
-    online_databases = ["Q212805", "Q1789476", "Q1916557", "Q7096323"]
+def is_instance_of_online_repository(response, wikidata_shortcode):
+    claims = response.get("entities", {}).get(wikidata_shortcode, {}).get("claims", {})
+    online_databases = ["Q212805", "Q1789476", "Q1916557", "Q7096323", "Q1235234", "Q45400320", "Q856234", "Q5281480", "Q45787211"]
     for db in online_databases:
         P31 = claims.get("P31", [])
         for P31_item in P31:
@@ -79,8 +79,8 @@ def is_instance_of_online_database(response):
     return False
 
 
-def is_instance_of_scientific_journal(response):
-    claims = response.get("claims", {})
+def is_instance_of_scientific_journal(response, wiki_shortcode):
+    claims = response.get("entities", {}).get(wiki_shortcode, {}).get("claims", {})
     P31 = claims.get("P31", [])
 
     for P31_item in P31:
@@ -93,8 +93,8 @@ def is_instance_of_scientific_journal(response):
     return False
 
 
-def has_issn_or_issnl(response):
-    claims = response.get("claims", {})
+def has_issn_or_issnl(response, wikidata_shortcode):
+    claims = response.get("entities", {}).get(wikidata_shortcode, {}).get("claims", {})
     P236 = claims.get("P236", [])
     P7363 = claims.get("P7363", [])
 
@@ -105,8 +105,8 @@ def has_issn_or_issnl(response):
     return False
 
 
-def has_source_words_in_description(response):
-    description = response.get("description", "").lower()
-    if "journal" in description or "repository" in description:
+def has_source_words_in_description(response, wikidata_shortcode):
+    description = response.get("entities", {}).get(wikidata_shortcode, {}).get("descriptions", {}).get("en", {}).get("value", "")
+    if "repository" in description.lower():
         print("Has journal or repository in description")
         return True
