a1a18f174a95e54de316b57217ba62cb2f0382e2
Richard Orr <richard@ourresearch.org>
2023-08-09 17:18:46 -0400

    add minimal logging to scripts.enqueue_works_with_changed_authors

--- scripts/enqueue_works_with_changed_authors.py
+++ scripts/enqueue_works_with_changed_authors.py
@@ -4,6 +4,7 @@ from redis import Redis
 
 from app import REDIS_QUEUE_URL
 from app import db
+from app import logger
 from scripts.fast_queue import REDIS_WORK_QUEUE
 
 _redis = Redis.from_url(REDIS_QUEUE_URL)
@@ -20,7 +21,9 @@ if __name__ == "__main__":
     paper_ids = [r[0] for r in db.session.query(WorkToEnqueue.paper_id).all()]
     if paper_ids:
         redis_queue_mapping = {paper_id: mktime(gmtime(0)) for paper_id in paper_ids}
-        if redis_queue_mapping:
-            _redis.zadd(REDIS_WORK_QUEUE, redis_queue_mapping)
-            WorkToEnqueue.query.delete()
-            db.session.commit()
+        logger.info(f'queueing {len(redis_queue_mapping)} works')
+        _redis.zadd(REDIS_WORK_QUEUE, redis_queue_mapping)
+        WorkToEnqueue.query.delete()
+        db.session.commit()
+    else:
+        logger.info('no works to queue')
