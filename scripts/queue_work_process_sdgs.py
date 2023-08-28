aaa2ae7d5a5ad5cf83011d53d663c3589b89bb27
Casey M <caseym@gmail.com>
2023-08-28 17:57:11 -0500

    improve sdg parser

--- scripts/queue_work_process_sdgs.py
+++ scripts/queue_work_process_sdgs.py
@@ -86,7 +86,7 @@ class QueueWorkProcessSdgs:
             while num_updated < limit:
                 start_time = time()
 
-                work_ids = self.fetch_queue_chunk(chunk_size)
+                work_ids = self.fetch_and_lock_queue_chunk(chunk_size)
 
                 if not work_ids:
                     logger.info('no queued Works ready to process... waiting.')
@@ -94,13 +94,6 @@ class QueueWorkProcessSdgs:
                     continue
 
                 works = QueueWorkProcessSdgs.fetch_works(work_ids)
-
-                db.session.execute('''
-                    UPDATE queue.run_once_work_process_sdgs 
-                    SET started = NOW() 
-                    WHERE work_id = any(:work_ids)
-                ''', {'work_ids': work_ids})
-                db.session.commit()
 
                 for work in works:
                     logger.info(f'running process_sdgs on {work}')
@@ -125,20 +118,28 @@ class QueueWorkProcessSdgs:
                 logger.info(f'processed {len(work_ids)} Works in {elapsed(start_time, 2)} seconds')
 
     @staticmethod
-    def fetch_queue_chunk(chunk_size):
+    def fetch_and_lock_queue_chunk(chunk_size):
         logger.info("looking for works to update sdgs on")
 
-        queue_query = text(f"""
-            SELECT work_id
-            FROM queue.run_once_work_process_sdgs
-            WHERE started IS NULL
-            LIMIT :chunk
-            FOR UPDATE SKIP LOCKED
-        """).bindparams(chunk=chunk_size)
+        with db.engine.begin() as connection:
+            queue_query = text(f"""
+                SELECT work_id
+                FROM queue.run_once_work_process_sdgs
+                WHERE started IS NULL
+                LIMIT :chunk
+                FOR UPDATE SKIP LOCKED
+            """).bindparams(chunk=chunk_size)
 
-        job_time = time()
-        id_list = [row[0] for row in db.engine.execute(queue_query.execution_options(autocommit=True)).all()]
-        logger.info(f'got {len(id_list)} IDs, took {elapsed(job_time)} seconds')
+            id_list = [row[0] for row in connection.execute(queue_query).all()]
+
+            # Immediately mark the fetched IDs as started within the same transaction
+            connection.execute("""
+                UPDATE queue.run_once_work_process_sdgs 
+                SET started = NOW() 
+                WHERE work_id = ANY(%(id_list)s)
+            """, {'id_list': id_list})
+
+        logger.info(f'got {len(id_list)} IDs to process')
 
         return id_list
 
@@ -174,7 +175,7 @@ class QueueWorkProcessSdgs:
 
 
 if __name__ == "__main__":
-    sleep(random.randint(0, 180))
+    sleep(random.randint(0, 60))
     parser = argparse.ArgumentParser()
     parser.add_argument('--id', nargs="?", type=str, help="id of the Work sdgs you want to update")
     parser.add_argument('--limit', "-l", nargs="?", type=int, help="how many Works to update")
