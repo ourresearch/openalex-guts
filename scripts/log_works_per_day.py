918bf71098ce2858bee1af60d0311637de8bfb0f
h1-the-swan <jason.portenoy@gmail.com>
2023-09-26 15:50:46 -0400

    email alert if no newly created or updated works

--- scripts/log_works_per_day.py
+++ scripts/log_works_per_day.py
@@ -5,6 +5,19 @@ import requests
 from app import db, logger
 from sqlalchemy import text
 
+def send_email(to_address, subject, body):
+    mailgun_api_key = os.getenv('MAILGUN_API_KEY')
+    mailgun_url = f"https://api.mailgun.net/v3/ourresearch.org/messages"
+    mailgun_auth = ("api", mailgun_api_key)
+    mailgun_data = {
+        "from": "OurResearch Mailgun <mailgun@ourresearch.org>",
+        "to": [to_address],
+        "subject": subject,
+        "text": body,
+    }
+    requests.post(mailgun_url, auth=mailgun_auth, data=mailgun_data)
+
+
 api_key = os.getenv('OPENALEX_API_KEY')
 
 yesterday = (
@@ -26,3 +39,15 @@ db.engine.execute(
         day=yesterday, created=num_created, updated=num_updated
     ).execution_options(autocommit=True)
 )
+
+if int(num_created) == 0 or int(num_updated) == 0:
+    # send warning email
+    to_address = "dev@ourresearch.org"
+    logger.info(f"sending email alert to {to_address}")
+    subject = f"ALERT OpenAlex No Created or Updated Works ({num_created} created, {num_updated} updated yesterday)"
+    body = '\n'.join([f'Filtering by created_date:{yesterday}, there were {num_created} works\n',
+            f'Filtering by updated_date:{yesterday}, there were {num_updated} works\n',
+            '\nThis is an automated alert sent out by the `log_works_per_day.py` script if there are either no created works or no updated works.\n',
+        ])
+    send_email(to_address, subject, body)
+    logger.info('sent email')
