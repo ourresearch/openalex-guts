5eae3a678ba806e047fb41c9be6ce64b0910215d
Richard Orr <richard@ourresearch.org>
2024-01-02 16:53:33 -0500

    add parseland recordthresher queue

---
+++ scripts/queue_make_parseland_rt_records.py
@@ -0,0 +1,125 @@
+import argparse
+import hashlib
+import json
+import uuid
+from time import sleep, time
+
+import requests
+import shortuuid
+from sqlalchemy import text
+
+from app import db
+from app import logger
+from models import Record
+from util import elapsed
+
+
+def _parseland_api_url(doi):
+    return f'https://parseland.herokuapp.com/parse-publisher?doi={doi}'
+
+
+def _record_id(doi):
+    return shortuuid.encode(
+        uuid.UUID(bytes=hashlib.sha256(f'parseland:{doi}'.encode('utf-8')).digest()[0:16])
+    )
+
+
+class QueueMakeParselandRTRecords:
+    @staticmethod
+    def queue_name():
+        return 'parseland_record_store'
+
+    def worker_run(self, **kwargs):
+        chunk_size = kwargs.get("chunk", 100)
+        limit = kwargs.get("limit", None)
+
+        if limit is None:
+            limit = float("inf")
+
+        num_updated = 0
+
+        while num_updated < limit:
+            start_time = time()
+
+            works = self.fetch_queue_chunk(chunk_size)
+
+            if not works:
+                logger.info('no queued records ready to fetch. waiting...')
+                sleep(60)
+                continue
+
+            for work in works:
+                logger.info(f'making record for {work}')
+                try:
+                    response = requests.get(_parseland_api_url(work['doi']), verify=False).json()
+                    authors = response.get('authors')
+                    pl_record = Record(
+                        id=_record_id(work['doi']),
+                        record_type='crossref_parseland',
+                        authors=authors and json.dumps(authors),
+                        published_date=response.get('published_date'),
+                        genre=response.get('genre'),
+                        abstract=response.get('abstract'),
+                        doi=work['doi']
+                    )
+                    db.session.merge(pl_record)
+                except:
+                    logger.error(f"couldn't make record for work['doi']")
+
+            db.session.execute(
+                text(f'''
+                    delete from queue.{QueueMakeParselandRTRecords.queue_name()} q
+                    where q.id = any(:work_ids)
+                ''').bindparams(work_ids=[w['id'] for w in works])
+            )
+
+            logger.info('committing changes')
+            commit_start_time = time()
+            db.session.commit()
+            logger.info(f'commit took {elapsed(commit_start_time, 2)} seconds')
+
+            logger.info(f'processed {len(works)} works in {elapsed(start_time, 2)} seconds')
+            num_updated += len(works)
+
+    @staticmethod
+    def fetch_queue_chunk(chunk_size):
+        logger.info("looking for records to fetch parseland records for")
+
+        queue_name = QueueMakeParselandRTRecords.queue_name()
+
+        queue_query = text(f"""
+            with queue_chunk as (
+                select id, doi
+                from queue.{queue_name}
+                where started is null
+                order by rand
+                limit :chunk
+                for update skip locked
+            )
+            update queue.{queue_name} q
+            set started = now()
+            from queue_chunk
+            where q.id = queue_chunk.id
+            returning q.id, q.doi;
+        """).bindparams(chunk=chunk_size)
+
+        job_time = time()
+        id_list = [
+            {'id': r[0], 'doi': r[1]}
+            for r in db.engine.execute(queue_query.execution_options(autocommit=True)).all()
+        ]
+        logger.info(f'got {len(id_list)} IDs, took {elapsed(job_time)} seconds')
+
+        return id_list
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--id', nargs="?", type=str, help="id of the Work you want to add_everything to")
+    parser.add_argument('--limit', "-l", nargs="?", type=int, help="how many Works to update")
+    parser.add_argument('--chunk', "-ch", nargs="?", default=10, type=int, help="how many Works to update at once")
+
+    parsed_args = parser.parse_args()
+
+    my_queue = QueueMakeParselandRTRecords()
+    my_queue.worker_run(**vars(parsed_args))
