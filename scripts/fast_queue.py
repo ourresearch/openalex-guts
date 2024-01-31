45d31e5482fc692c890095703311a45888e1f840
Casey M <caseym@gmail.com>
2024-01-31 15:43:47 -0600

    set up topics endpoint

--- scripts/fast_queue.py
+++ scripts/fast_queue.py
@@ -368,6 +368,8 @@ def get_objects(entity_type, object_ids):
         ).filter(models.Publisher.publisher_id.in_(object_ids)).all()
     elif entity_type == "funder":
         objects = db.session.query(models.Funder).filter(models.Funder.funder_id.in_(object_ids)).all()
+    elif entity_type == "topic":
+        objects = db.session.query(models.Topic).filter(models.Topic.topic_id.in_(object_ids)).all()
     logger.info(f'got {len(objects)} objects in {elapsed(start_time, 4)}s')
     return objects
 
