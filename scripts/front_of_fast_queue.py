5e75de0db4b8863565a00dd28f068aeb23255c03
h1-the-swan <jason.portenoy@gmail.com>
2024-02-28 09:31:23 -0500

    refactor front_of_fast_queue

--- scripts/front_of_fast_queue.py
+++ scripts/front_of_fast_queue.py
@@ -10,6 +10,16 @@ from models import REDIS_WORK_QUEUE
 _redis = Redis.from_url(REDIS_QUEUE_URL)
 
 
+def front_of_fast_queue_one_batch(work_ids, no_redis=False):
+    """Adds a list of work IDs to the redis fast queue as one batch.
+    `work_ids` is a list of integers
+    `if `no_redis` is True: return batch but don't actually make the change to the fast queue
+    Returns the batch (dict used to add to redis queue)"""
+    batch = {work_id: -1 for work_id in work_ids}
+    if (batch) and (no_redis is False):
+        _redis.zadd(REDIS_WORK_QUEUE, batch)
+    return batch
+
 def front_of_fast_queue(file_name, batch_size, no_redis=False):
     """
     Takes a list of work IDs in a CSV and moves them to the front of the fast queue.
@@ -22,29 +32,27 @@ def front_of_fast_queue(file_name, batch_size, no_redis=False):
         count = 0
 
         reader = csv.reader(f)
-        batch = {}
+        work_ids_for_fast_queue = []
 
         # iterate through the CSV file in batches
         for i, row in enumerate(reader):
-            if i % batch_size == 0 and batch:
+            if i % batch_size == 0 and work_ids_for_fast_queue:
+                batch = front_of_fast_queue_one_batch(work_ids_for_fast_queue, no_redis=no_redis)
                 if no_redis:
                     print(batch)
-                else:
-                    _redis.zadd(REDIS_WORK_QUEUE, batch)
-                batch = {}
+                work_ids_for_fast_queue = []
                 print(f"processed {count} rows")
                 sleep(0.5)
 
             work_id = int(row[0].replace("https://openalex.org/W", ""))
-            batch[work_id] = -1
+            work_ids_for_fast_queue.append(work_id)
             count += 1
 
         # add remaining items in the batch if any
-        if batch:
+        if work_ids_for_fast_queue:
+            batch = front_of_fast_queue_one_batch(work_ids_for_fast_queue, no_redis=no_redis)
             if no_redis:
                 print(batch)
-            else:
-                _redis.zadd(REDIS_WORK_QUEUE, batch)
             print(f"finished processing file, count is {count}")
 
 
