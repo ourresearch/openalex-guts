22e331bec6b1102a1f1e0825300c21a2d44dc7c9
Casey M <caseym@gmail.com>
2024-04-11 16:42:09 +0100

    add works count and citation count to keywords

--- models/keyword.py
+++ models/keyword.py
@@ -8,15 +8,17 @@ from app import get_apiurl_from_openalex_url
 from app import logger
 from app import KEYWORDS_INDEX
 from bulk_actions import create_bulk_actions
+from models.counts import citation_count_from_elastic, works_count_from_elastic
 
 
 def as_keyword_openalex_id(id):
     from app import API_HOST
+
     return f"{API_HOST}/keywords/{id}"
 
 
 class Keyword(db.Model):
-    __table_args__ = {'schema': 'mid'}
+    __table_args__ = {"schema": "mid"}
     __tablename__ = "keyword"
 
     keyword_id = db.Column(db.Text, primary_key=True)
@@ -36,6 +38,7 @@ class Keyword(db.Model):
     @property
     def openalex_id_short(self):
         from models import short_openalex_id
+
         return short_openalex_id(self.openalex_id)
 
     @property
@@ -51,25 +54,37 @@ class Keyword(db.Model):
         response = {
             "id": self.openalex_id,
             "display_name": self.display_name,
-            "keyword": self.display_name
+            "keyword": self.display_name,
         }
         if return_level == "full":
-            response.update({
-                # "siblings": self.siblings,
-                # "works_count": int(self.counts.paper_count or 0) if self.counts else 0,
-                # "cited_by_count": int(self.counts.citation_count or 0) if self.counts else 0,
-                "works_api_url": f"https://api.openalex.org/works?filter=keywords.id:{self.openalex_id_short}",
-                "updated_date": datetime.datetime.utcnow().isoformat(),
-                "created_date": self.created_date.isoformat()[0:10] if isinstance(self.created_date, datetime.datetime) else self.created_date[0:10]
-            })
+            response.update(
+                {
+                    "works_count": works_count_from_elastic(
+                        "keywords.id.keyword", self.openalex_id
+                    ),
+                    "cited_by_count": citation_count_from_elastic(
+                        "keywords.id.keyword", self.openalex_id
+                    ),
+                    "works_api_url": f"https://api.openalex.org/works?filter=keywords.id:{self.openalex_id_short}",
+                    "updated_date": datetime.datetime.utcnow().isoformat(),
+                    "created_date": self.created_date.isoformat()[0:10]
+                    if isinstance(self.created_date, datetime.datetime)
+                    else self.created_date[0:10],
+                }
+            )
+        print(response)
         return response
 
     def __repr__(self):
-        return "<Keyword ( {} ) {} {}>".format(self.openalex_api_url, self.id, self.display_name)
+        return "<Keyword ( {} ) {} {}>".format(
+            self.openalex_api_url, self.id, self.display_name
+        )
 
 
 logger.info(f"loading valid keyword IDs")
-_valid_keywords = db.session.query(Keyword.keyword_id).options(orm.Load(Keyword).raiseload('*')).all()
+_valid_keywords = (
+    db.session.query(Keyword.keyword_id).options(orm.Load(Keyword).raiseload("*")).all()
+)
 _valid_keyword_ids = set([k.keyword_id for k in _valid_keywords])
 
 
