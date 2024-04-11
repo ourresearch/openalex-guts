b6a0fa0106b0d79f604a57ebdba06d7a76e9e58c
Casey M <caseym@gmail.com>
2024-04-11 14:07:40 +0100

    store keywords in api

--- models/keyword.py
+++ models/keyword.py
@@ -6,9 +6,8 @@ from sqlalchemy import orm
 from app import db
 from app import get_apiurl_from_openalex_url
 from app import logger
-# from app import KEYWORDS_INDEX
-import models
-from util import entity_md5
+from app import KEYWORDS_INDEX
+from bulk_actions import create_bulk_actions
 
 
 def as_keyword_openalex_id(id):
@@ -22,6 +21,7 @@ class Keyword(db.Model):
 
     keyword_id = db.Column(db.Text, primary_key=True)
     display_name = db.Column(db.Text)
+    json_entity_hash = db.Column(db.Text)
     updated_date = db.Column(db.DateTime)
     created_date = db.Column(db.DateTime)
 
@@ -42,43 +42,9 @@ class Keyword(db.Model):
     def openalex_api_url(self):
         return get_apiurl_from_openalex_url(self.openalex_id)
 
-    # @property
-    # def siblings(self):
-    #     siblings_query = (
-    #         db.session.query(models.Topic)
-    #         .filter(models.Topic.subfield_id == self.subfield_id)
-    #         .all()
-    #     )
-    #     topics_list = [
-    #         {'id': topic.openalex_id, 'display_name': topic.display_name}
-    #         for topic in siblings_query
-    #         if topic.topic_id != self.topic_id
-    #     ]
-    #     topics_list_sorted = sorted(topics_list, key=lambda x: x['display_name'].lower())
-    #     return topics_list_sorted
-
     def store(self):
-        bulk_actions = []
-
-        my_dict = self.to_dict()
-        my_dict['updated'] = my_dict.get('updated_date')
-        my_dict['@timestamp'] = datetime.datetime.utcnow().isoformat()
-        my_dict['@version'] = 1
-        entity_hash = entity_md5(my_dict)
-        old_entity_hash = self.json_entity_hash
-
-        if entity_hash != old_entity_hash:
-            logger.info(f"dictionary for {self.openalex_id} new or changed, so save again")
-            index_record = {
-                "_op_type": "index",
-                # "_index": KEYWORDS_INDEX,
-                "_id": self.openalex_id,
-                "_source": my_dict
-            }
-            bulk_actions.append(index_record)
-        else:
-            logger.info(f"dictionary not changed, don't save again {self.openalex_id}")
-        self.json_entity_hash = entity_hash
+        bulk_actions, new_entity_hash = create_bulk_actions(self, KEYWORDS_INDEX)
+        self.json_entity_hash = new_entity_hash
         return bulk_actions
 
     def to_dict(self, return_level="full"):
