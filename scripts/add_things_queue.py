9c74c2ff340be259228c43aabe1deb6bc32a49f5
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-04-17 22:50:17 -0500

    Use redis for add_most_things processing

---
+++ scripts/add_things_queue.py
@@ -0,0 +1,80 @@
+import json
+from datetime import datetime
+from time import mktime, gmtime
+
+from redis.client import Redis
+
+import models
+from app import REDIS_QUEUE_URL, logger
+from models import REDIS_ADD_THINGS_QUEUE
+from scripts.works_query import base_works_query
+
+_redis = Redis.from_url(REDIS_QUEUE_URL)
+
+CHUNK_SIZE = 50
+
+
+def enqueue_jobs(work_ids, priority=None, methods=None):
+    if methods is None:
+        methods = []
+    if not priority:
+        priority = mktime(gmtime(0))
+    mapping = {json.dumps({'work_id': work_id, 'methods': methods}): priority
+               for work_id in work_ids}
+    _redis.zadd(REDIS_ADD_THINGS_QUEUE, mapping)
+
+
+def enqueue_job(work_id, priority=None, methods=None):
+    enqueue_jobs([work_id], priority, methods)
+
+
+def dequeue_chunk(chunk_size):
+    items = _redis.zpopmin(REDIS_ADD_THINGS_QUEUE, chunk_size)
+    return [json.loads(item[0]) for item in items]
+
+
+def main():
+    total_processed = 0
+    start = datetime.now()
+    while True:
+        try:
+            jobs = dequeue_chunk(CHUNK_SIZE)
+        except Exception as e:
+            logger.info('Exception during dequeue, exiting...')
+            logger.exception(e)
+            break
+        jobs_map = {job['work_id']: job for job in jobs}
+        works = base_works_query().filter(
+                models.Work.paper_id.in_([job['work_id'] for job in jobs])
+            ).all()
+
+        for work in works:
+            job = jobs_map[work.paper_id]
+            if not job['methods']:
+                job['methods'] = ['add_everything']
+            for method_name in job['methods']:
+                method = getattr(work, method_name)
+                args = []
+                if method_name == 'add_everything':
+                    args = [True]
+                try:
+                    method(*args)
+                except Exception as e:
+                    logger.info(f'Exception calling {method_name}() on work {work.paper_id}')
+                    logger.exception(e)
+                    # Re-queue job
+                    enqueue_job(work.paper_title, 1e9, job['methods'])
+            total_processed += 1
+        now = datetime.now()
+        hrs_diff = (now - start).total_seconds() / (60 * 60)
+        rate = round(total_processed / hrs_diff, 2)
+        logger.info(f'Total processed: {total_processed} | Rate: {rate}/hr')
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
