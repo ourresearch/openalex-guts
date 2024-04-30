e5f7c247d1bffad3a9b8b5c240f3f0ad9ca3a7a0
h1-the-swan <jason.portenoy@gmail.com>
2024-04-30 11:13:00 -0400

    log updates and created per day for more entities

--- scripts/log_works_per_day.py
+++ scripts/log_works_per_day.py
@@ -20,34 +20,41 @@ def send_email(to_address, subject, body):
 
 api_key = os.getenv('OPENALEX_API_KEY')
 
-yesterday = (
-    datetime.datetime.utcnow().date() - datetime.timedelta(days=1)
-).isoformat()
+entities = [
+    "works", "authors", "sources", "institutions", "topics", "funders", "publishers"
+]
 
-num_created = requests.get(
-    f'https://api.openalex.org/works?filter=from_created_date:{yesterday}&api_key={api_key}'
-).json().get('meta').get('count')
+for e in entities:
 
-num_updated = requests.get(
-    f'https://api.openalex.org/works?filter=from_updated_date:{yesterday}&api_key={api_key}'
-).json().get('meta').get('count')
+    yesterday = (
+        datetime.datetime.utcnow().date() - datetime.timedelta(days=1)
+    ).isoformat()
 
-logger.info({'day': yesterday, 'created': num_created, 'updated': num_updated})
+    num_created = requests.get(
+        f'https://api.openalex.org/{e}?filter=from_created_date:{yesterday}&api_key={api_key}'
+    ).json().get('meta').get('count')
 
-db.engine.execute(
-    text('insert into log.works_per_day (day, num_created, num_updated) values (:day, :created, :updated)').bindparams(
-        day=yesterday, created=num_created, updated=num_updated
-    ).execution_options(autocommit=True)
-)
+    num_updated = requests.get(
+        f'https://api.openalex.org/{e}?filter=from_updated_date:{yesterday}&api_key={api_key}'
+    ).json().get('meta').get('count')
 
-if int(num_created) == 0 or int(num_updated) == 0:
-    # send warning email
-    to_address = "dev@ourresearch.org"
-    logger.info(f"sending email alert to {to_address}")
-    subject = f"ALERT OpenAlex No Created or Updated Works ({num_created} created, {num_updated} updated yesterday)"
-    body = '\n'.join([f'Filtering by created_date:{yesterday}, there were {num_created} works\n',
-            f'Filtering by updated_date:{yesterday}, there were {num_updated} works\n',
-            '\nThis is an automated alert sent out by the `log_works_per_day.py` script if there are either no created works or no updated works.\n',
-        ])
-    send_email(to_address, subject, body)
-    logger.info('sent email')
+    if e == "works":
+        logger.info({'day': yesterday, 'created': num_created, 'updated': num_updated})
+
+    db.engine.execute(
+        text(f'insert into log.{e}_per_day (day, num_created, num_updated) values (:day, :created, :updated)').bindparams(
+            day=yesterday, created=num_created, updated=num_updated
+        ).execution_options(autocommit=True)
+    )
+
+    if e == "works" and (int(num_created) == 0 or int(num_updated) == 0):
+        # send warning email
+        to_address = "dev@ourresearch.org"
+        logger.info(f"sending email alert to {to_address}")
+        subject = f"ALERT OpenAlex No Created or Updated Works ({num_created} created, {num_updated} updated yesterday)"
+        body = '\n'.join([f'Filtering by created_date:{yesterday}, there were {num_created} works\n',
+                f'Filtering by updated_date:{yesterday}, there were {num_updated} works\n',
+                '\nThis is an automated alert sent out by the `log_works_per_day.py` script if there are either no created works or no updated works.\n',
+            ])
+        send_email(to_address, subject, body)
+        logger.info('sent email')
