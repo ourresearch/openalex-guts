12597847e2288945715f58d1362860c4f2cee742
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-06-20 11:34:58 -0500

    Add enqueue from OpenAlex API filter to front_of_fast_queue.py

--- scripts/front_of_fast_queue.py
+++ scripts/front_of_fast_queue.py
@@ -6,6 +6,7 @@ from redis import Redis
 
 from app import REDIS_QUEUE_URL
 from models import REDIS_WORK_QUEUE
+from util import openalex_works_paginate
 
 _redis = Redis.from_url(REDIS_QUEUE_URL)
 
@@ -19,6 +20,7 @@ def front_of_fast_queue_one_batch(work_ids, no_redis=False):
     if (batch) and (no_redis is False):
         _redis.zadd(REDIS_WORK_QUEUE, batch)
     return batch
+
 
 def front_of_fast_queue(file_name, batch_size, no_redis=False):
     """
@@ -37,7 +39,8 @@ def front_of_fast_queue(file_name, batch_size, no_redis=False):
         # iterate through the CSV file in batches
         for i, row in enumerate(reader):
             if i % batch_size == 0 and work_ids_for_fast_queue:
-                batch = front_of_fast_queue_one_batch(work_ids_for_fast_queue, no_redis=no_redis)
+                batch = front_of_fast_queue_one_batch(work_ids_for_fast_queue,
+                                                      no_redis=no_redis)
                 if no_redis:
                     print(batch)
                 work_ids_for_fast_queue = []
@@ -50,17 +53,51 @@ def front_of_fast_queue(file_name, batch_size, no_redis=False):
 
         # add remaining items in the batch if any
         if work_ids_for_fast_queue:
-            batch = front_of_fast_queue_one_batch(work_ids_for_fast_queue, no_redis=no_redis)
+            batch = front_of_fast_queue_one_batch(work_ids_for_fast_queue,
+                                                  no_redis=no_redis)
             if no_redis:
                 print(batch)
             print(f"finished processing file, count is {count}")
 
 
+def front_of_fast_queue_api(oax_filter, batch_size, no_redis=False):
+    batch = []
+    count = 0
+    for page in openalex_works_paginate(oax_filter, select='id'):
+        for work in page:
+            count += 1
+            try:
+                work_id = int(work['id'].split('/W')[-1])
+                batch.append(work_id)
+                if len(batch) == batch_size:
+                    front_of_fast_queue_one_batch(batch, no_redis=no_redis)
+                    print(
+                        f'Enqueued {len(batch)} ({count} total) works to front of fast queue from filter: {oax_filter}')
+                    batch.clear()
+            except Exception as e:
+                print(e)
+    front_of_fast_queue_one_batch(batch, no_redis=no_redis)
+    print(
+        f'Enqueued {len(batch)} ({count} total) works to front of fast queue from filter: {oax_filter}')
+    batch.clear()
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Front of Fast Queue Script")
-    parser.add_argument("--file", required=True, help="Input CSV file name")
-    parser.add_argument("--batch-size", type=int, default=10000, help="Batch size (default: 10000)")
-    parser.add_argument("--no-redis", action="store_true", help="Test script without saving to Redis")
+    parser.add_argument('--oax_filter', '-oaxf',
+                        help='OpenAlex API filter(s) to enqueue works',
+                        action='append')
+    parser.add_argument("--file", help="Input CSV file name")
+    parser.add_argument("--batch-size", type=int, default=10000,
+                        help="Batch size (default: 10000)")
+    parser.add_argument("--no-redis", action="store_true",
+                        help="Test script without saving to Redis")
     args = parser.parse_args()
-
-    front_of_fast_queue(args.file, args.batch_size, args.no_redis)
+    if not args.oax_filter and not args.file:
+        raise ValueError('Either --oax_filter or --file must be specified')
+    if args.file:
+        front_of_fast_queue(args.file, args.batch_size, args.no_redis)
+    else:
+        for _filter in args.oax_filter:
+            print(f'Enqueueing works from OpenAlex filter: {_filter}')
+            front_of_fast_queue_api(_filter, args.batch_size, args.no_redis)
