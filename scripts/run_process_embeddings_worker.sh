c1455a3258dd2cc8075e23d3a42b1b28ab439178
Casey M <caseym@gmail.com>
2024-06-27 12:03:02 -0400

    decrease chunk size

--- scripts/run_process_embeddings_worker.sh
+++ scripts/run_process_embeddings_worker.sh
@@ -1,6 +1,6 @@
 for (( i=1; i<=$PROCESS_EMBEDDINGS_WORKERS_PER_DYNO; i++ ))
 do
-  COMMAND="python -m scripts.queue_work_process_embeddings --chunk=100"
+  COMMAND="python -m scripts.queue_work_process_embeddings --chunk=50"
   echo $COMMAND
   $COMMAND&
 done
