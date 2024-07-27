c05d9b9eb5547a6746548913f1efa21c6b1bcd45
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-07-27 16:28:25 -0500

    Read from Redis queue rather than ins.mag_authors table when upserting

--- upsert_mag_records.py
+++ upsert_mag_records.py
@@ -1,19 +1,22 @@
 from datetime import datetime
-
 from sqlalchemy import text
 
-from app import db
+import redis
 import shortuuid
 
+from app import db, REDIS_QUEUE_URL
 from scripts.helpers.enqueue_add_some_things import enqueue_works
+
+UPSERT_QUEUE = 'queue:mag_authors_upsert'
+REDIS = redis.from_url(REDIS_QUEUE_URL)
+BATCH_SIZE = 1_000
 
 
 def make_recordthresher_id():
     return shortuuid.uuid()[:22]
 
 
-def update_or_insert_record(session, row):
-    # Define the update query
+def update_or_insert_record(session, work_id):
     update_query = """
         UPDATE ins.recordthresher_record
         SET authors = mag_authors.authors
@@ -23,58 +26,40 @@ def update_or_insert_record(session, row):
           AND ins.recordthresher_record.work_id = :work_id;
     """
 
-    # Execute the update query
-    result = session.execute(text(update_query), {'work_id': row['work_id']})
+    result = session.execute(text(update_query), {'work_id': work_id})
 
-    # If no rows were updated, perform the insert
-    if result.rowcount == 0 and row['authors']:
+    if result.rowcount == 0:
+        record_id = make_recordthresher_id()
         insert_query = """
             INSERT INTO ins.recordthresher_record (id, work_id, record_type, authors)
-            VALUES (:id, :work_id, 'mag_location', :authors);
+            SELECT :id, :work_id, 'mag_location', mag_authors.authors
+            FROM ins.mag_authors
+            WHERE work_id = :work_id;
         """
-        update = {'id': make_recordthresher_id()}
-        update.update(row)
-        session.execute(text(insert_query), update)
+        session.execute(text(insert_query),
+                        {'id': record_id, 'work_id': work_id})
+
     session.commit()
 
 
-def dequeue_and_mark_processing(session, batch_size=100):
-    cte_query = """
-        WITH rows_to_process AS (
-            SELECT *
-            FROM ins.mag_authors
-            WHERE (processing IS NULL OR processing = FALSE)
-            ORDER BY work_id
-            LIMIT :batch_size
-            FOR UPDATE SKIP LOCKED
-        )
-        UPDATE ins.mag_authors
-        SET processing = TRUE
-        FROM rows_to_process
-        WHERE ins.mag_authors.work_id = rows_to_process.work_id
-        RETURNING rows_to_process.*;
-    """
-
-    result = session.connection().execution_options(autocommit=True).execute(text(cte_query), {'batch_size': batch_size})
-    return result.mappings().all()
+def dequeue_work_ids(num):
+    work_ids_bytes = REDIS.spop(UPSERT_QUEUE, num)
+    return [int(work_id.decode('utf-8')) for work_id in work_ids_bytes]
 
 
 if __name__ == '__main__':
-    last_work_id = None
     count = 0
     start = datetime.now()
     while True:
-        rows = dequeue_and_mark_processing(db.session)
-        if not rows:
-            break
+        work_ids = dequeue_work_ids(BATCH_SIZE)
 
-        for row in rows:
-            update_or_insert_record(db.session, row)
+        for work_id in work_ids:
+            update_or_insert_record(db.session, work_id)
             count += 1
-        last_work_id = rows[-1].work_id
+        last_work_id = work_ids[-1]
         mark_updated_query = '''UPDATE ins.mag_authors SET finished = true WHERE work_id IN :work_ids'''
-        work_ids = [row['work_id'] for row in rows]
-        db.session.execute(text(mark_updated_query), {'work_ids': tuple(work_ids)})
+        db.session.execute(text(mark_updated_query),
+                           {'work_ids': tuple(work_ids)})
         enqueue_works(work_ids, methods=None, fast_queue_priority=-1)
         now = datetime.now()
         elapsed_hrs = (now - start).total_seconds() / 3600
