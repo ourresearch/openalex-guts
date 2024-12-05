0c78ae03f49f1f2a0e4b8341059ce0126faef0ea
Justin Barrett <jpbarret13@gmail.com>
2024-12-05 17:00:30 -0500

    Updating work keywords

--- models/work_keyword.py
+++ models/work_keyword.py
@@ -3,14 +3,14 @@ from app import db
 
 class WorkKeyword(db.Model):
     __table_args__ = {'schema': 'mid'}
-    __tablename__ = "work_keyword"
+    __tablename__ = "work_keyword_concept"
 
     paper_id = db.Column(db.BigInteger, db.ForeignKey("mid.work.paper_id"), primary_key=True)
     keyword_id = db.Column(db.Text, db.ForeignKey("mid.keyword.keyword_id"), primary_key=True)
     score = db.Column(db.Float)
-    algorithm_version = db.Column(db.Integer)
+    algorithm = db.Column(db.Integer)
     updated = db.Column(db.DateTime)
-    keywords_input_hash = db.Column(db.Text)
+    keyword_input_hash = db.Column(db.Text)
 
     def to_dict(self, return_level="full"):
         response = self.keyword.to_dict(return_level)
