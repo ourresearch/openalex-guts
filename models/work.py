ef6ffa0174b0e361aca829f660052e3761eb2692
Nolan McCafferty <nolanmccafferty@gmail.com>
2024-12-08 06:31:09 -0600

    Check if lineage exists

--- models/work.py
+++ models/work.py
@@ -1984,7 +1984,7 @@ class Work(db.Model):
         top_publisher_blacklist = {4310320990, 4310319965}
         publisher_str = (self.journal and self.journal.publisher) or self.publisher
         lineage = self.dict_locations and self.dict_locations[0].get('source') and self.dict_locations[0]['source']['host_organization_lineage']
-        host_org_blacklisted = any([int(re.search(r'\d+$', org_id).group(0)) in top_publisher_blacklist for org_id in lineage])
+        host_org_blacklisted = any([int(re.search(r'\d+$', org_id).group(0)) in top_publisher_blacklist for org_id in lineage]) if lineage else False
         if (publisher_str and ('springer' in publisher_str.lower() or 'elsevier' in publisher_str.lower())) or host_org_blacklisted:
             return not self.is_oa
         return False
