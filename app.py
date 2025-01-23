a6db535091cce6107d984e4e8d6b9b9448a039ba
Casey M <caseym@gmail.com>
2025-01-23 17:33:21 -0600

    switch to authors v16

--- app.py
+++ app.py
@@ -176,7 +176,7 @@ sentry_sdk.init(
 )
 
 # indexes
-AUTHORS_INDEX = "authors-v15"
+AUTHORS_INDEX = "authors-v16"
 CONCEPTS_INDEX = "concepts-v8"
 CONTINENTS_INDEX = "continents-v1"
 COUNTRIES_INDEX = "countries-v2"
